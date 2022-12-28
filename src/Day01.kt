fc75274f178a9487303b61e102f8ddb678ef7181
Jacques Cornat <cornat.jacques@gmail.com>
2022-12-28 15:05:58 +0100

    finish day 03

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,28 +12,28 @@ fun main() {
     // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    val mutableList = mutableListOf<Int>();
-    var weight = 0;
+    val mutableList = mutableListOf<Int>()
+    var weight = 0
     for (a in input) {
         if (a == "") {
-            mutableList.add(weight);
-            weight = 0;
-            continue;
+            mutableList.add(weight)
+            weight = 0
+            continue
         }
 
-        weight += a.toInt();
+        weight += a.toInt()
     }
 
-    mutableList.add(weight); // When loop is over, add the last elf
+    mutableList.add(weight) // When loop is over, add the last elf
 
-    mutableList.sortDescending();
-    val totalCalories = mutableList.first();
+    mutableList.sortDescending()
+    val totalCalories = mutableList.first()
     println("How many total Calories is that Elf carrying? $totalCalories")
 
-    var top3 = 0;
+    var top3 = 0
     mutableList.slice(0 .. 2)
         .forEach {
-            top3 += it;
+            top3 += it
         }
 
     println("How many Calories are those Elves carrying in total? $top3")
--- src/Day03.kt
+++ src/Day03.kt
@@ -1,49 +1,75 @@
 fun main() {
+    fun charToInt(char: Char): Int {
+        val charCode = char.code
+        var convertedInt = 0
+        if (charCode < 97) {
+            convertedInt = charCode - 64 + 26 //
+        } else {
+            convertedInt = charCode - 96 //
+        }
+
+        return convertedInt
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            val sizePer2 = line.length / 2
+            var leftPart = line.slice(0..sizePer2 - 1)
+            var rightPart = line.slice(sizePer2..line.length - 1)
+
+            var mutableList = mutableListOf<Char>()
+            for (char in leftPart) {
+                if (!rightPart.contains(char)) {
+                    continue
+                }
+
+                if (mutableList.contains(char)) {
+                    continue
+                }
+
+                mutableList.add(char)
+                sum += charToInt(char);
+            }
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var matchingList = mutableListOf<Char>()
+        for (i in input.indices) {
+            val isFirstLine = (i % 3) == 0;
+            if (!isFirstLine) {
+                continue
+            }
+
+            val firstLine = input[i];
+            val secondLine = input[i + 1];
+            val thirdLine = input[i + 2]
+
+            for (char in firstLine) {
+                if (secondLine.contains(char) && thirdLine.contains(char)) {
+                    matchingList.add(char)
+                    break
+                }
+            }
+        }
+
+        var sum = 0
+        for (char in matchingList) {
+            sum += charToInt(char)
+        }
+
+        return sum
     }
 
     val input = readInput("Day03")
-    val testInput = readInput("Day03_test")
-    var sum = 0;
-    for (item in input) {
-        val sizePer2 = item.length / 2;
-        var leftPart = item.slice(0..sizePer2 - 1);
-        var rightPart = item.slice(sizePer2..item.length - 1);
+//    val input = readInput("Day03_test")
 
-        val x = leftPart.find { it.equals('J') }
-        var mutableList = mutableListOf<Char>();
-        for (char in leftPart) {
-            if (!rightPart.contains(char)) {
-                continue;
-            }
+    val sum = part1(input)
+    println("What is the sum of the priorities of those item types? $sum")
 
-            if (mutableList.contains(char)) {
-                continue;
-            }
-
-            mutableList.add(char);
-//            println("$char ${char.code}" )
-            val charCode = char.code;
-            var convertedInt = 0;
-            if (charCode < 97) {
-                convertedInt = charCode - 64 + 26; //
-            } else {
-                convertedInt = charCode - 96; //
-            }
-
-            sum += convertedInt;
-        }
-//        val y = leftPart.indexOf('J')
-//        println("$item : $leftPart - $rightPart")
-    }
-
-    println("sum $sum")
-
-//    val input = readInput("Day03")
-
+    val sum2 = part2(input)
+    println("What is the sum of the priorities of those item types? $sum2")
 }
