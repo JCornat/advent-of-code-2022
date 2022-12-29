dd0a495ea4b1ccb9d52f4ac652192c4f24055c96
Jacques Cornat <cornat.jacques@gmail.com>
2022-12-29 16:47:22 +0100

    finish day 02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,40 +1,84 @@
 fun main() {
-    fun translateLetterToShape(letter: Char): String {
+    fun translateLetterToShape(letter: String): String {
         var res = ""
         when (letter) {
-            'A', 'X' -> res = "Rock"
-            'B', 'Y' -> res = "Paper"
-            'C', 'Z' -> res = "Scissor"
+            "A", "X" -> res = "Rock"
+            "B", "Y" -> res = "Paper"
+            "C", "Z" -> res = "Scissor"
         }
+
+        check(res != "")
 
         return res
     }
 
-    fun evaluate(opponent: String, you: String): String {
+    fun translateLetterToOutcome(letter: String): String {
         var res = ""
-        when (opponent) {
-            you -> {
+        when (letter) {
+            "X" -> res = "Lose"
+            "Y" -> res = "Draw"
+            "Z" -> res = "Win"
+        }
+
+        check(res != "")
+
+        return res
+    }
+
+    fun evaluateOutcome(opponentShape: String, playerShape: String): String {
+        var res = ""
+        when (opponentShape) {
+            playerShape -> {
                 res = "Draw"
             }
             "Rock" -> {
-                when (you) {
-                    "Paper" -> "Win"
-                    "Scissor" -> "Lose"
+                when (playerShape) {
+                    "Paper" -> res = "Win"
+                    "Scissor" -> res = "Lose"
                 }
             }
             "Paper" -> {
-                when (you) {
-                    "Scissor" -> "Win"
-                    "Rock" -> "Lose"
+                when (playerShape) {
+                    "Scissor" -> res = "Win"
+                    "Rock" -> res = "Lose"
                 }
             }
             "Scissor" -> {
-                when (you) {
-                    "Rock" -> "Win"
-                    "Paper" -> "Lose"
+                when (playerShape) {
+                    "Rock" -> res = "Win"
+                    "Paper" -> res = "Lose"
                 }
             }
         }
+
+        check(res != "")
+
+        return res;
+    }
+
+    fun evaluateShapeFromOutcome(opponentShape: String, outcome: String): String {
+        var res = ""
+        when (outcome) {
+            "Draw" -> {
+                res = opponentShape
+            }
+            "Lose" -> {
+                when (opponentShape) {
+                    "Rock" -> res = "Scissor"
+                    "Paper" -> res = "Rock"
+                    "Scissor" -> res = "Paper"
+                }
+            }
+            "Win" -> {
+                when (opponentShape) {
+                    "Rock" -> res = "Paper"
+                    "Paper" -> res = "Scissor"
+                    "Scissor" -> res = "Rock"
+                }
+            }
+        }
+
+        check(res != "")
 
         return res;
     }
@@ -55,25 +99,50 @@ fun main() {
         return res
     }
 
-    fun parseShapes(line: String): (String, String) {
-        line.split(" ")
+    fun parseShapes(line: String): Pair<String, String> {
+        val list = line.split(" ")
+        val leftLetter = list.first()
+        val rightLetter = list.last()
+        val opponentShape = translateLetterToShape(leftLetter)
+        val playerShape = translateLetterToShape(rightLetter)
+        return Pair(opponentShape, playerShape)
+    }
+
+    fun parseShapes2(line: String): Pair<String, String> {
+        val list = line.split(" ")
+        val leftLetter = list.first()
+        val rightLetter = list.last()
+        val opponentShape = translateLetterToShape(leftLetter)
+        val outcome = translateLetterToOutcome(rightLetter)
+        val playerShape = evaluateShapeFromOutcome(opponentShape, outcome)
+        return Pair(playerShape, outcome)
     }
 
     fun part1(input: List<String>): Int {
         var score = 0
         for (line in input) {
-            val opponentShape
+            val (opponentShape, playerShape) = parseShapes(line)
+
+            val outcome = evaluateOutcome(opponentShape, playerShape)
+            score += calculateScore(playerShape, outcome)
         }
 
         return score
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var score = 0
+        for (line in input) {
+            val (playerShape, outcome) = parseShapes2(line)
+
+            score += calculateScore(playerShape, outcome)
+        }
+
+        return score
     }
 
-//    val input = readInput("Day02")
-    val input = readInput("Day02_test")
+    val input = readInput("Day02")
+//    val input = readInput("Day02_test")
 
     val score = part1(input)
     println("What would your total score be if everything goes exactly according to your strategy guide? $score")
