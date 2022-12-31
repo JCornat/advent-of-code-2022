23adc1e0dcb247292bad3ef41fb406594f35fd18
Jacques Cornat <cornat.jacques@gmail.com>
2022-12-31 16:34:49 +0100

    update day 05 input parsing

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,40 +1,66 @@
 fun main() {
+    data class Step (val count: Int, val source: Int, val target: Int)
+
     fun getStackWidth(input: String): Int {
         return (input.length + 1) / 4
     }
 
-    fun parseStackLine(input: String, size: Int): MutableList<String> {
-        var stackLine = (List(size) { "" }).toMutableList()
-        for (i in 0..size - 1) {
-            val startIndex = i * 4
-            val x = input.slice(startIndex..startIndex + 2)
-            val regex = """\[([A-Z])\]""".toRegex()
-            val result = regex.find(x)
-            if (result?.value == null) {
+    fun formatStacks(stacks: MutableList<MutableList<String>>, columnCount: Int): MutableList<MutableList<String>> {
+        var res = mutableListOf<MutableList<String>>()
+        for (stack in stacks) {
+            val difference = columnCount - stack.size
+            if (difference == 0) {
                 continue
             }
 
-            stackLine.set(i, result!!.groupValues.last())
+            check(difference > 0)
+
+            var stackLine = (List(difference) { "" }).toMutableList()
+            stack.addAll(stackLine)
+        }
+
+        return res
+    }
+
+    fun parseStackLine(input: String): MutableList<String> {
+        var stackLine = mutableListOf<String>()
+        var stackWidth = getStackWidth(input)
+        for (i in 0..stackWidth - 1) {
+            val startIndex = i * 4
+            val character = input.slice(startIndex..startIndex + 2)
+            val regex = """\[([A-Z])\]""".toRegex()
+            val result = regex.find(character)
+            if (result?.value == null) {
+                stackLine.add("")
+                continue
+            }
+
+            stackLine.add(result!!.groupValues.last())
         }
 
         return stackLine.toMutableList()
     }
 
-    fun parseStepLine(input: String): MutableList<Pair<Int, Int>> {
+    fun parseStepLine(input: String): Step {
         val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
         val result = regex.find(input)
         if (result?.groupValues == null) {
             check(true == false)
         }
 
-        var steps = mutableListOf<Pair<Int, Int>>()
-        val iterations = result!!.groupValues[1].toInt()
-        for (i in 0..iterations) {
-            val pair = Pair(result!!.groupValues[2].toInt(), result!!.groupValues[3].toInt())
-            steps.add(pair)
-        }
+        val count = result!!.groupValues[1].toInt()
+        val source = result!!.groupValues[2].toInt()
+        val target = result!!.groupValues[3].toInt()
+        return Step(count, source, target)
+    }
 
-        return steps
+    fun parseColumnCount(input: String): Int {
+        val regex = """(\d+)""".toRegex()
+        val matches = regex.findAll(input)
+            .map { it.value }
+            .toList()
+
+        return matches.last().toInt()
     }
 
     fun getLineType(input: String): String {
@@ -43,27 +69,32 @@ fun main() {
             res = "step"
         } else if (input.contains("[")) {
             res = "stack"
+        } else if (input.contains(" 1 ")) {
+            res = "row"
         }
 
         return res
     }
 
-    fun parseInput(input: List<String>): Pair<MutableList<MutableList<String>>, MutableList<Pair<Int, Int>>> {
-        val stackWidth = getStackWidth(input.first())
+    fun parseInput(input: List<String>): Pair<MutableList<MutableList<String>>, MutableList<Step>> {
         var stacks = mutableListOf<MutableList<String>>()
-        var steps = mutableListOf<Pair<Int, Int>>()
+        var steps = mutableListOf<Step>()
         for (line in input) {
             val lineType = getLineType(line)
             when (lineType) {
                 "stack" -> {
-                    val stackLine = parseStackLine(line, stackWidth)
+                    val stackLine = parseStackLine(line)
                     stacks.add(stackLine)
                 }
+                "row" -> {
+                    val columnCount = parseColumnCount(line)
+                    formatStacks(stacks, columnCount)
+                }
                 "step" -> {
-                    val stepLines = parseStepLine(line)
-                    for (stepLine in stepLines) {
-                        steps.add(stepLine)
-                    }
+                    check(stacks.size > 0)
+
+                    val step = parseStepLine(line)
+                    steps.add(step)
                 }
             }
         }
@@ -90,8 +121,8 @@ fun main() {
 //    val input = readInput("Day05")
     val input = readInput("Day05_test")
 
-    val totalContains = part1(input)
-    println("In how many assignment pairs does one range fully contain the other? $totalContains")
+    val topStack = part1(input)
+    println("After the rearrangement procedure completes, what crate ends up on top of each stack? $topStack")
 
     val totalOverlaps = part2(input)
     println("In how many assignment pairs do the ranges overlap? $totalOverlaps")
