51b0a818d7c3b24c907def2e81dcbe1b26e56945
Jacques Cornat <cornat.jacques@gmail.com>
2023-01-01 14:39:00 +0100

    end day 06

--- src/Day06.kt
+++ src/Day06.kt
@@ -14,12 +14,40 @@ fun main() {
         return false
     }
 
-    fun getFirstMarkerPosition(line: String): Int {
+    fun getStartPacketPosition(line: String): Int {
+        val limit = 4
         var position = 0
         var markerList = mutableListOf<Char>()
         for (i in 0..line.length - 1) {
             val character: Char = line.get(i)
-            if (markerList.size < 3) {
+            if (markerList.size < limit - 1) {
+                markerList.add(character)
+                continue
+            }
+
+            markerList.add(character)
+            if (!hasRepetition(markerList)) {
+                position = i + 1
+                break
+            }
+
+            markerList.removeFirst()
+        }
+
+        if (position == 0) {
+            return -1
+        }
+
+        return position
+    }
+
+    fun getStartMessagePosition(line: String): Int {
+        val limit = 14
+        var position = 0
+        var markerList = mutableListOf<Char>()
+        for (i in 0..line.length - 1) {
+            val character: Char = line.get(i)
+            if (markerList.size < limit - 1) {
                 markerList.add(character)
                 continue
             }
@@ -43,22 +71,29 @@ fun main() {
     fun part1(input: List<String>): Int {
         var count = 0
         for (line in input) {
-            count = getFirstMarkerPosition(line)
+            count = getStartPacketPosition(line)
         }
 
         return count
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var count = 0
+        for (line in input) {
+            count = getStartMessagePosition(line)
+            println(count)
+        }
+
+        return count
     }
 
-//    val input = readInput("Day06")
-    val input = readInput("Day06_test2")
+    val input = readInput("Day06")
+//    val input = readInput("Day06_test")
+//    val input = readInput("Day06_test2")
 
     val count = part1(input)
     println("How many characters need to be processed before the first start-of-packet marker is detected? $count")
 
     val sum2 = part2(input)
-    println("What is the sum of the priorities of those item types? $sum2")
+    println("How many characters need to be processed before the first start-of-message marker is detected? $sum2")
 }
